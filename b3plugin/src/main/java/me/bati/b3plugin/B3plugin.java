96b59a224d64c371d101550a20e9f83b5b75a955
Bati <batiulutas@gmail.com>
2023-08-30 23:43:09 +0300

    project created

---
+++ b3plugin/src/main/java/me/bati/b3plugin/B3plugin.java
@@ -0,0 +1,17 @@
+package me.bati.b3plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class B3plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
