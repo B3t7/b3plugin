d2f65f99b2d52af846c011f78d06b8591ae37a73
Bati <batiulutas@gmail.com>
2023-09-01 01:44:28 +0300

    added commands package and b3give command java class

--- b3plugin/src/main/java/me/bati/b3plugin/B3plugin.java
+++ b3plugin/src/main/java/me/bati/b3plugin/B3plugin.java
@@ -7,13 +7,19 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.swing.*;
 import java.util.Collections;
+import java.util.Objects;
+
+import static org.bukkit.event.block.Action.RIGHT_CLICK_AIR;
 
 public final class B3plugin extends JavaPlugin implements Listener {
 
@@ -51,6 +57,7 @@ public final class B3plugin extends JavaPlugin implements Listener {
         Location joinloc = p.getLocation();
         GameMode pgmd = p.getGameMode();
         Inventory jinv = p.getInventory();
+        Boolean isop = event.getPlayer().isOp();
 
         Double xlocdb = joinloc.getX();
         Double ylocdb = joinloc.getY();
@@ -63,12 +70,28 @@ public final class B3plugin extends JavaPlugin implements Listener {
         String strjoinloc = (ChatColor.GREEN + "[ "+ xloc+ " " + yloc +" "+ zloc + " ]");
         event.setJoinMessage(pstr + " joined at "+ strjoinloc + " welcome!");
 
-        if (pstr.equals("B3t7") && pgmd == GameMode.CREATIVE ){
+        if (isop && pgmd == GameMode.CREATIVE ){
             jinv.addItem(lgrod);
         }
 
 
 
+
+    }
+    @EventHandler
+    public void onPlayerLgrodRC(PlayerInteractEvent event){
+        AddLgrod();
+        Location rcpos = event.getPlayer().getTargetBlock(null,512).getLocation();
+        ItemMeta itmm = Objects.requireNonNull(event.getItem()).getItemMeta();
+
+        event.getAction();
+        if(event.getAction() == Action.RIGHT_CLICK_AIR){
+            if(itmm != null && itmm.equals(lgrod.getItemMeta())){
+                event.getPlayer().getWorld().strikeLightning(rcpos);
+            }
+
+
+        }
 
     }
 }
